8d7632b5127dede2398c5715a82816fb001ca828
ismailkarakayax <ismlkrky58@gmail.com>
2024-03-11 00:32:02 +0300

    delete user-review-service address crud

--- user-review-service/src/main/java/com/n11/userreviewservice/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.n11.userreviewservice.repository;
-
-import com.n11.userreviewservice.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address,Long> {
-}
