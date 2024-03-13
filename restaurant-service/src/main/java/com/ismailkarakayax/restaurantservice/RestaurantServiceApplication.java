cb9d9c26e7fb63af06b405cb7cadeda56b6684bf
ismailkarakayax <ismlkrky58@gmail.com>
2024-03-13 16:34:41 +0300

    add eureka client configuration

--- restaurant-service/src/main/java/com/ismailkarakayax/restaurantservice/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/com/ismailkarakayax/restaurantservice/RestaurantServiceApplication.java
@@ -2,10 +2,12 @@ package com.ismailkarakayax.restaurantservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.solr.repository.config.EnableSolrRepositories;
 
 @SpringBootApplication
 @EnableSolrRepositories
+@EnableDiscoveryClient
 public class RestaurantServiceApplication {
 
 	public static void main(String[] args) {
